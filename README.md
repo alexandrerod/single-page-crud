733831c732d41939094ebd2006af4871b7ab4507
alexandrerod <nrodriguesalexandre@gmail.com>
2020-11-05 15:12:23 -0300

    migrações criadas

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # single-page-crud
+
+Objetivo é criar uma 
+única página que contenha todo CRUD do app.
--- example/models.py
+++ example/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -5,11 +7,17 @@ from django.db import models
 class OrdemDeServico(models.Model):
     nome = models.CharField(verbose_name='Nome', max_length=255)
     tag = models.CharField(max_length=20)
-    data_incial = models.DateField(verbose_name='Data de Iníco', auto_now=True)
-    data_final = models.DateField(verbose_name='Data Final', auto_now=True)
+    data_incial = models.DateField(verbose_name='Data de Iníco', default=datetime.date.today)
+    data_final = models.DateField(verbose_name='Data Final', default=datetime.date.today)
     descricao = models.TextField(verbose_name='Descrição do Serviço', help_text='Descreva o serviço à ser realizado')
 
 
     def __str__(self):
         return f'{self.nome} - {self.tag}'
+
+
+    class Meta:
+        verbose_name = 'Ordem de serviço'
+        verbose_name_plural = 'Ordens de Serviços'
+
 
